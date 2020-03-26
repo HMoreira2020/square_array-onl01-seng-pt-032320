a316eea07f910722cefa3cfe8503c575141faaf4
Heather Moreira <moreiraheather@gmail.com>
2020-03-26 03:11:12 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,5 @@
 def square_array
-  new_array = []square_array.each {|number| number**2, new_array.push(number)}
+  new_array = []
+  square_array.each {|number| new_array.push(number ** 2)}
   return new_array
 end
